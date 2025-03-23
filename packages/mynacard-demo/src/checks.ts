932ab2e8ead77b48c97ba3858f4ab7e73c691a9a
314systems <77012577+314systems@users.noreply.github.com>
2025-03-23 16:55:59 +0900

    checks.ts を更新

--- packages/mynacard-demo/src/checks.ts
+++ packages/mynacard-demo/src/checks.ts
@@ -1,6 +1,5 @@
 import { BasicTLVParser, SchemaParser } from "@aokiapp/tlv-parser";
 import {
-  hexStringToUint8Array,
   schemaCertificate,
   schemaKenhojoBasicFour,
   schemaKenhojoSignature,
@@ -8,87 +7,166 @@ import {
   schemaKenkakuMyNumber,
 } from "@aokiapp/mynacard";
 
-export const kenkakuEntries = hexStringToUint8Array(
-  process.env.KENKAKU_02 || "",
-);
+function hexStringToUint8Array(hexString: string): Uint8Array<ArrayBuffer> {
+  // Check if the input is in hexadecimal format
+  if (!/^[0-9a-fA-F\s]+$/.test(hexString)) {
+    throw new Error("Invalid hexadecimal string: Contains non-hex characters.");
+  }
 
-export const kenkakuInformation = hexStringToUint8Array(
-  process.env.KENKAKU_03 || "",
-);
+  // Remove whitespace
+  hexString = hexString.replace(/\s+/g, "");
 
-export const kenkakuCertificate = hexStringToUint8Array(
-  process.env.KENKAKU_04 || "",
-);
+  // Check if the length is even
+  if (hexString.length % 2 !== 0) {
+    throw new Error("Invalid hexadecimal string: Length must be even.");
+  }
 
-export const kenkakuMyNumber = hexStringToUint8Array(
-  process.env.KENKAKU_05 || "",
-);
+  // Create Uint8Array
+  const uint8Array = new Uint8Array(hexString.length / 2);
 
-export const kenhojoMyNumber = hexStringToUint8Array(
-  process.env.KENHOJO_01 || "",
-);
+  // Convert two characters at a time
+  for (let i = 0; i < hexString.length; i += 2) {
+    uint8Array[i / 2] = Number("0x" + hexString.slice(i, i + 2));
+  }
 
-export const kenhojoBasicFour = hexStringToUint8Array(
-  process.env.KENHOJO_02 || "",
-);
+  return uint8Array;
+}
 
-export const kenhojoSignature = hexStringToUint8Array(
-  process.env.KENHOJO_03 || "",
-);
+function arrayBufferToBase64(buffer: ArrayBuffer): string {
+  if (typeof Buffer !== "undefined") {
+    return Buffer.from(buffer).toString("base64");
+  } else {
+    const bytes = new Uint8Array(buffer);
+    let binary = "";
+    for (const byte of bytes) binary += String.fromCharCode(byte);
+    const base64 = btoa(binary);
+    return base64;
+  }
+}
 
-export const kenhojoCertificate = hexStringToUint8Array(
-  process.env.KENHOJO_04 || "",
-);
+function uint8ArrayToHexString(uint8Array: Uint8Array): string {
+  return Array.from(uint8Array)
+    .map((byte) => byte.toString(16).padStart(2, "0"))
+    .join("");
+}
 
-export const kenhojoInformation = hexStringToUint8Array(
-  process.env.KENHOJO_05 || "",
-);
+const {
+  // KENKAKU_01, // 誕生日,
+  KENKAKU_02, // 券面内容
+  // KENKAKU_03, // AP基本情報
+  KENKAKU_04, // 証明書
+  KENKAKU_05, // 個人番号写真
+  // KENKAKU_06, // 不明
+  KENHOJO_01, // 個人番号
+  KENHOJO_02, // 基本4情報
+  KENHOJO_03, // 署名
+  KENHOJO_04, // 証明書
+  // KENHOJO_05, // AP基本情報
+  // KENHOJO_06, // 不明
+  // KENHOJO_07, // 認証鍵
+  // KENHOJO_08, // 不明
+} = process.env;
 
-export const kenhojoEf0006 = hexStringToUint8Array(
-  process.env.KENHOJO_06 || "",
-);
+// const kenkakuBirth = KENKAKU_01 && hexStringToUint8Array(KENKAKU_01);
+const kenkakuEntries = KENKAKU_02 && hexStringToUint8Array(KENKAKU_02);
+// const kenkakuInformation = KENKAKU_03 && hexStringToUint8Array(KENKAKU_03);
+const kenkakuCertificate = KENKAKU_04 && hexStringToUint8Array(KENKAKU_04);
+const kenkakuMyNumber = KENKAKU_05 && hexStringToUint8Array(KENKAKU_05);
+// const kenkakuEf0006 = KENKAKU_06 && hexStringToUint8Array(KENKAKU_06);
 
-export const kenhojoAuthKey = hexStringToUint8Array(
-  process.env.KENHOJO_07 || "",
-);
+const kenhojoMyNumber = KENHOJO_01 && hexStringToUint8Array(KENHOJO_01);
+const kenhojoBasicFour = KENHOJO_02 && hexStringToUint8Array(KENHOJO_02);
+const kenhojoSignature = KENHOJO_03 && hexStringToUint8Array(KENHOJO_03);
+const kenhojoCertificate = KENHOJO_04 && hexStringToUint8Array(KENHOJO_04);
+// const kenhojoInformation = KENHOJO_05 && hexStringToUint8Array(KENHOJO_05);
+// const kenhojoEf0006 = KENHOJO_06 && hexStringToUint8Array(KENHOJO_06);
+// const kenhojoAuthKey = KENHOJO_07 && hexStringToUint8Array(KENHOJO_07);
 
-const kenhojoMyNumberParsed = BasicTLVParser.parse(kenhojoMyNumber.buffer);
-console.log("=== KENHOJO MY NUMBER DATA ===");
-console.log(kenhojoMyNumberParsed.value);
-console.log("=== KENHOJO MY NUMBER DATA ===");
+if (kenkakuEntries) {
+  const parser = new SchemaParser(schemaKenkakuEntries);
+  const parsed = await parser.parse(kenkakuEntries.buffer, { async: true });
+  console.log("=== KENKAKU ENTRIES (02) DATA ===");
+  console.log("Offsets:", parsed.offsets);
+  console.log("Birth:", parsed.birth);
+  console.log("Gender:", parsed.gender);
+  const nameBase64 = arrayBufferToBase64(parsed.namePng.buffer);
+  console.log(`Name: data:image/png;base64,${nameBase64}`);
+  const addressBase64 = arrayBufferToBase64(parsed.addressPng.buffer);
+  console.log(`Address: data:image/png;base64,${addressBase64}`);
+  const faceBase64 = arrayBufferToBase64(parsed.faceJp2.buffer);
+  console.log(`Face: data:image/jp2;base64,${faceBase64}`);
+  console.log("Expire:", parsed.expire);
+  const securityCodeBase64 = arrayBufferToBase64(parsed.securityCodePng.buffer);
+  console.log(`Security Code: data:image/png;base64,${securityCodeBase64}`);
+  console.log("=== KENKAKU ENTRIES (02) DATA ===");
+}
 
-const kenhojoBasicFourParser = new SchemaParser(schemaKenhojoBasicFour);
-console.log("=== KENHOJO BASIC FOUR DATA ===");
-console.log(kenhojoBasicFourParser.parse(kenhojoBasicFour.buffer));
-console.log("=== KENHOJO BASIC FOUR DATA ===");
+console.log();
 
-const kenhojoSignatureParser = new SchemaParser(schemaKenhojoSignature);
-console.log("=== KENHOJO SIGNATURE DATA ===");
-console.log(kenhojoSignatureParser.parse(kenhojoSignature.buffer));
-console.log("=== KENHOJO SIGNATURE DATA ===");
+if (kenkakuCertificate) {
+  const parser = new SchemaParser(schemaCertificate);
+  const parsed = await parser.parse(kenkakuCertificate.buffer, { async: true });
+  console.log("=== KENKAKU CERTIFICATE (04) DATA ===");
+  console.log(parsed);
+  console.log("=== KENKAKU CERTIFICATE (04) DATA ===");
+}
 
-const kenhojoCertificateParser = new SchemaParser(schemaCertificate);
-const kenhojoCertificateParsed = await kenhojoCertificateParser.parse(
-  kenhojoCertificate.buffer,
-  { async: true },
-);
-console.log("=== KENHOJO CERTIFICATE DATA ===");
-console.log(kenhojoCertificateParsed);
-console.log("=== KENHOJO CERTIFICATE DATA ===");
+console.log();
 
-const kenkakuEntriesParser = new SchemaParser(schemaKenkakuEntries);
-const kenkakuEntriesParsed = kenkakuEntriesParser.parse(kenkakuEntries.buffer, {
-  async: true,
-});
-console.log("=== KENKAKU ENTRIES DATA ===");
-console.log(kenkakuEntriesParsed);
-console.log("=== KENKAKU ENTRIES DATA ===");
+if (kenkakuMyNumber) {
+  const parser = new SchemaParser(schemaKenkakuMyNumber);
+  const parsed = await parser.parse(kenkakuMyNumber.buffer, { async: true });
+  console.log("=== KENKAKU MY NUMBER (05) DATA ===");
+  const myNumberBase64 = arrayBufferToBase64(parsed.myNumberPng.buffer);
+  console.log(`data:image/png;base64,${myNumberBase64}`);
+  console.log("=== KENKAKU MY NUMBER (05) DATA ===");
+}
 
-const kenkakuMyNumberParser = new SchemaParser(schemaKenkakuMyNumber);
-const kenkakuMyNumberParsed = kenkakuMyNumberParser.parse(
-  kenkakuMyNumber.buffer,
-  { async: true },
-);
-console.log("=== KENKAKU MY NUMBER DATA ===");
-console.log(kenkakuMyNumberParsed);
-console.log("=== KENKAKU MY NUMBER DATA ===");
+console.log();
+
+if (kenhojoMyNumber) {
+  const parsed = BasicTLVParser.parse(kenhojoMyNumber.buffer);
+  console.log("=== KENHOJO MY NUMBER (01) DATA ===");
+  console.log(new TextDecoder().decode(parsed.value));
+  console.log("=== KENHOJO MY NUMBER (01) DATA ===");
+}
+
+console.log();
+
+if (kenhojoBasicFour) {
+  const parser = new SchemaParser(schemaKenhojoBasicFour);
+  const parsed = parser.parse(kenhojoBasicFour.buffer);
+  console.log("=== KENHOJO BASIC FOUR (02) DATA ===");
+  console.log(`Name: ${parsed.name}`);
+  console.log(`Address: ${parsed.address}`);
+  console.log(`Birth: ${parsed.birth}`);
+  console.log(`Gender: ${parsed.gender}`);
+  console.log("=== KENHOJO BASIC FOUR (02) DATA ===");
+}
+
+console.log();
+
+if (kenhojoSignature) {
+  const parser = new SchemaParser(schemaKenhojoSignature);
+  console.log("=== KENHOJO SIGNATURE (03) DATA ===");
+  const parsed = parser.parse(kenhojoSignature.buffer);
+  console.log(
+    "Kenhojo My Number Signature:",
+    uint8ArrayToHexString(parsed.kenhojoMyNumberSignature),
+  );
+  console.log(
+    "Kenhojo Basic Four Signature:",
+    uint8ArrayToHexString(parsed.kenhojoAttributesSignature),
+  );
+  console.log("=== KENHOJO SIGNATURE (03) DATA ===");
+}
+
+console.log();
+
+if (kenhojoCertificate) {
+  const parser = new SchemaParser(schemaCertificate);
+  const parsed = await parser.parse(kenhojoCertificate.buffer, { async: true });
+  console.log("=== KENHOJO CERTIFICATE (04) DATA ===");
+  console.log(parsed);
+  console.log("=== KENHOJO CERTIFICATE (04) DATA ===");
+}
