9a366bb5d74087284180c094dc49c195a3642a3c
314systems <77012577+314systems@users.noreply.github.com>
2025-03-23 10:02:53 +0900

    ESLint の設定を更新

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,22 @@
 // @ts-check
 
-import eslint from '@eslint/js';
+import eslint from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
-import tseslint from 'typescript-eslint';
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
-  tseslint.configs.recommended,
-  eslintConfigPrettier
+  tseslint.configs.recommendedTypeChecked,
+  eslintConfigPrettier,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    ignores: ["**/dist/**"],
+  },
 );
